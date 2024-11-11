2ca8eeb9cb9eaab8fbbfc7de64ec76a5fa5a1beb
Emmanuel Michael Ikechukwu <162467554+Emmanuel-Michael05@users.noreply.github.com>
2024-11-11 18:37:20 +0100

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Globalization;
+
+namespace Examples
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+                Console.WriteLine("Enter a number   ");
+                int userInput = int.Parse(Console.ReadLine());
+  
+            for (int i = 1; i <= 10; i++)
+            {
+                int result = i * userInput;
+                Console.WriteLine( $"{i} * {userInput} = {result}");
+                Console.ReadLine();
+            }
+        }
+    }
+
+  
+}
---
+++ ConsoleApp6/hookup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp6
+{
+    static class hookup
+    {
+        public static void sexChat(string name) 
+        {
+            Console.WriteLine($"{name} sexchat is 4k");
+        }
+        public static void nudePics(string name) 
+        {
+            Console.WriteLine($"{name} nude pics is 2k");
+        }
+    }
+}
